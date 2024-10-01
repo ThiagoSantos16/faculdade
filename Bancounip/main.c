e256ad6ca01bec09c8444d9ce01bf291e04d6529
Eduardo Lisboa <eduardo.c.lisboa@gmail.com>
2024-10-01 18:55:58 -0300

    Identação

--- Bancounip/main.c
+++ Bancounip/main.c
@@ -1,110 +1,96 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-  int saldo=200;
-int deposito=0;
-int saque=0;
-int opcao=0;
-char nome[30];
+int main() {
+    int saldo = 200;
+    int deposito = 0;
+    int saque = 0;
+    int opcao = 0;
+    char nome[30];
 
-system("clear");
+    system("clear");
 
-printf("Digite seu nome:");
-scanf("%s", nome);
+    printf("Digite seu nome: ");
+    scanf("%s", nome);
 
-system ("clear");
+    system("clear");
 
-printf("\n");
-printf("*** Bem vindo ao Bank ***\n");
-printf("             %s", nome);
-printf("\n");
-printf("\n");
- printf("Digite 1 para saque \n");
-printf("Digite 2 para deposito\n");
-printf("\n");
-printf("\n"); 
- printf("Opcao:");
-scanf("%d", &opcao);
+    printf("\n");
+    printf("*** Bem vindo ao Bank ***\n");
+    printf("             %s", nome);
+    printf("\n");
+    printf("\n");
+    printf("Digite 1 para saque \n");
+    printf("Digite 2 para deposito\n");
+    printf("\n");
+    printf("\n"); 
+    printf("Opcao: ");
+    scanf("%d", &opcao);
 
-if 
-(opcao == 1){
-system("clear");
+    if (opcao == 1) {
+        system("clear");
 
-printf("\n");
-printf("\n");
-printf("*** Voce selecionou a  opcao SAQUE***/n");
-printf("           saldo atual: %d", saldo);
-printf("\n");
-printf("\n");
-printf("Qual valor que deseja SACAR ?");
-printf("\n");
-printf("\n");
-printf("Valor do saque:");
-scanf("%d", &saque);
-  
-if(saque > saldo || saque <0)
-{
+        printf("\n");
+        printf("\n");
+        printf("*** Voce selecionou a opcao SAQUE ***\n");
+        printf("           saldo atual: %d", saldo);
+        printf("\n");
+        printf("\n");
+        printf("Qual valor que deseja SACAR?\n");
+        printf("\n");
+        printf("Valor do saque: ");
+        scanf("%d", &saque);
 
-system("clear");
-printf("\n");
-printf("\n");
-printf("\n");
-printf("     *** Saldo insuficiente!!! ***            ");
+        if (saque > saldo || saque < 0) {
+            system("clear");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("     *** Saldo insuficiente!!! ***\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+        } else {
+            saldo = saldo - saque;
 
-printf("\n");
-printf("\n");
-printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+
+            system("clear");
+            printf("\n");
+            printf("\n");
+            printf("     *** Seu saldo atual e de: %d ***", saldo);
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+        }
+    }
+
+    if (opcao == 2) {
+        system("clear");
+        printf("\n");
+        printf("\n");
+        printf("*** Voce selecionou a opcao DEPOSITO ***\n");
+        printf("             Saldo atual: %d", saldo);
+        printf("\n");
+        printf("\n");
+        printf("\n");
+        printf("Digite o valor que deseja DEPOSITAR: ");
+        scanf("%d", &deposito);
+        saldo = saldo + deposito; // Alternatively: saldo += deposito;
+
+        system("clear");
+
+        printf("\n");
+        printf("\n");
+        printf("      *** Seu saldo atual e de: %d ***", saldo);
+        printf("\n");
+        printf("\n");
+        printf("\n");
+    }
+
+    return 0;
 }
-
-
-else  
-  {
-saldo= saldo - saque;
-
-printf("\n");
-printf("\n");
-printf("\n");
-
-system ("clear");
-printf("\n");
-printf("\n");
-printf("     *** Seu saldo atual e de : %d", saldo);
-
-printf("***");
-printf("\n");
-printf("\n");
-printf("\n");
-printf("\n");
-printf("\n");
-}
-}
-if( opcao == 2)
-{
-system("clear");
-printf("\n");
-printf("\n");
-printf("*** Voce selecionou a opcao DEPOSITO ***/n");
-printf("             Saldo atual: %d", saldo);
-printf("\n");
-printf("\n");
-printf("\n");
-printf("Digite o valor que deseja DEPOSITAR: ");
-scanf("%d", &deposito);
-saldo = saldo + deposito;
-/*saldo+= deposito  */
-
-system ("clear");
-
-printf("\n");
-printf("\n");
-printf("      *** Seu saldo atual e de: %d", saldo);
-printf("***");
-printf("\n");
-printf("\n");
-printf("\n");
-
-}
-}
-
