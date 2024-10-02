e0792b3d19a20a49be4504b346dcb5ebd696e2ab
Eduardo Lisboa <eduardo.c.lisboa@gmail.com>
2024-10-02 15:49:30 -0300

    indentação

--- CalculoMédiaNotas/main.c
+++ CalculoMédiaNotas/main.c
@@ -1,28 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(void)
+{
 
-int main (void) {
+    float NP1, NP2, PIM, MEDIA;
 
-
-float NP1, NP2, PIM, MEDIA;
-
-
-printf(" Digite a nota da NP1 (peso 4)\n");
+    printf(" Digite a nota da NP1 (peso 4)\n");
 scanf ("%f",&NP1);
 printf ("Digite a nota da NP2 (peso 4)\n");
 scanf ("%f",&NP2);
 printf ("Digite a nota do PIM (peso 2)\n");
 scanf ("%f", &PIM);
 
-MEDIA= (NP1 * 4 + NP2 * 4 + PIM * 2) / ( 4 + 4 + 2);
-printf("A media e %.f \n", MEDIA);
+    MEDIA = (NP1 * 4 + NP2 * 4 + PIM * 2) / (4 + 4 + 2);
+    printf("A media e %.f \n", MEDIA);
 
-if (MEDIA>=7){
-printf("Aluno aprovado.\n");
-}else{
-printf ("Aluno de exame.\n");
-}
+    if (MEDIA >= 7)
+    {
+        printf("Aluno aprovado.\n");
+    }
+    else
+    {
+        printf("Aluno de exame.\n");
+    }
 
-return 0;
+    return 0;
 }
